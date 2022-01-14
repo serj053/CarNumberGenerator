c66fbbb2c777b36c332dcccb565bf8024e2fe142
serj053 <serj053@yandex.ru>
2022-01-14 16:14:50 +0300

    Домашняя задание 7.9. Вывод рускоязычных символов. Доработка, заменил перебор на проверку интервала ожидаемых значений и добавил обрезание цикла после вывода нужных символов.

--- 07_9_IntOrInteger/src/Main.java
+++ 07_9_IntOrInteger/src/Main.java
@@ -3,27 +3,18 @@ public class Main {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
-        short n = 1;
+        byte n = 1;
         for (int i = 0; i < 65636; i++) {
             char ch = (char) i;
 
-            if (ch == 'а' || ch == 'б' || ch == 'в' || ch == 'г' || ch == 'д' || ch == 'е' ||
-                    ch == 'ё' || ch == 'ж' || ch == 'з' || ch == 'и' || ch == 'й' || ch == 'л' ||
-                    ch == 'к' || ch == 'м' || ch == 'н' || ch == 'о' || ch == 'п' || ch == 'р' ||
-                    ch == 'т' || ch == 'с' || ch == 'у' || ch == 'ф' || ch == 'ц' || ch == 'ч' ||
-                    ch == 'щ' || ch == 'ш' || ch == 'ь' || ch == 'ъ' || ch == 'э' || ch == 'ю' ||
-                    ch == 'я' || ch == 'А' || ch == 'Б' || ch == 'В' || ch == 'Г' || ch == 'Д' ||
-                    ch == 'Е' || ch == 'Ё' || ch == 'Ж' || ch == 'З' || ch == 'И' || ch == 'Л' ||
-                    ch == 'К' || ch == 'М' || ch == 'Н' || ch == 'О' || ch == 'П' || ch == 'Р' ||
-                    ch == 'Т' || ch == 'С' || ch == 'У' || ch == 'Ф' || ch == 'Ц' || ch == 'Ч' ||
-                    ch == 'Щ' || ch == 'Ш' || ch == 'Ь' || ch == 'Ы' || ch == 'Ъ' || ch == 'Э' ||
-                    ch == 'Ю' || ch == 'Я'
-            ) {
+            if (ch >= 'А' && ch <= 'я' || ch == 'ё' || ch == 'Ё') {
                 System.out.print(ch + ", ");
-                if(n % 10 == 0)
+                if (n % 10 == 0)
                     System.out.println();
                 n++;
             }
+            if(n > 66)
+                break;
         }
 
     }
