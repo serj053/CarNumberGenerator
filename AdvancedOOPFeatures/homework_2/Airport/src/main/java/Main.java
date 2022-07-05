0797fccaf876372b3e8c700f552fde93f518af57
serj053 <serj053@yandex.ru>
2022-07-05 21:52:03 +0300

    Практическая работа 13.12 исправление кода - использование  stream() в место for()

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -9,50 +9,42 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static com.skillbox.airport.Flight.Type.DEPARTURE;
+
 public class Main {
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
-        List<Terminal> tt = airport.getTerminals();
-        List<Flight> f = tt.get(0).getFlights();
-        System.out.println("getType " + f.get(0).getType());
 
-        List<Flight> getFlight = new ArrayList<>();
-        getFlight = findPlanesLeavingInTheNextTwoHours(airport);
+        List<Flight> getFlights ;
+        getFlights = findPlanesLeavingInTheNextTwoHours(airport);
 
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy.MM.dd  HH:mm.ss");
-        for (Flight fly : getFlight) {
+        for (Flight fly : getFlights) {
             System.out.println(fly);
         }
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должен вернуть список рейсов вылетающих в ближайшие два часа.
-        List<Terminal> terminal = airport.getTerminals();
-        List<Flight> sum = new ArrayList<>();//
-        //собираем все данные о вылетах с разных терминалов в один список
-        for (Terminal value : terminal) {
-            sum.addAll(value.getFlights());
-        }
-
-        //готовим граничные переменные
-        LocalDateTime startTime = LocalDateTime.now();
-        LocalDateTime endTime = startTime.plusHours(2);
-        //контейнер для сбора результата
         List<Flight> flights = new ArrayList<>();
-        for (Flight flt : sum) {
-            LocalDateTime flightTime = flt.getDate().toInstant()
-                    .atZone(ZoneId.systemDefault())
-                    .toLocalDateTime();
-
-            if ((flightTime.isEqual(startTime) || flightTime.isEqual(endTime)) ||
-                    (flightTime.isBefore(endTime) && flightTime.isAfter(startTime)
-                            && flt.getType().toString().equals("DEPARTURE"))) {
-
-                flights.add(flt);
-            }
-        }
+        airport.getTerminals().forEach(t -> t.getFlights().stream()
+                .filter(f -> f.getType().toString().equals("DEPARTURE"))
+                .filter(f -> {
+                    LocalDateTime startTime = LocalDateTime.now();
+                    LocalDateTime endTime = startTime.plusHours(2);
+                    LocalDateTime flightTime = f.getDate().toInstant()
+                            .atZone(ZoneId.systemDefault())
+                            .toLocalDateTime();
+                    if ((flightTime.isEqual(startTime) || flightTime.isEqual(endTime)) ||
+                            (flightTime.isBefore(endTime) && flightTime.isAfter(startTime)
+                                    && f.getType().toString().equals("DEPARTURE"))) {
+                        flights.add(f);
+                    }
+                    return false;
+                }).forEach(flights::add));
         return flights;
     }
-
 }
+
+/*
+*
+* */
